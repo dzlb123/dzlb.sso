75850a0b764b0ffdb258014d9c77fc8f032ed397
wuhaitao <wuhaitao@bdfint.com>
2017-12-22 17:20:30 +0800

    add test class

--- src/main/java/com/dzlb/sso/controller/UserController.java
+++ src/main/java/com/dzlb/sso/controller/UserController.java
@@ -1,8 +1,20 @@
 package com.dzlb.sso.controller;
 
+import com.dzlb.sso.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
 /**
  * Created by wuhai on 2017/12/20.
  */
-
+@RestController
+@RequestMapping("/user")
 public class UserController {
+
+    @Autowired
+    private UserService userService;
+
+
+
 }
