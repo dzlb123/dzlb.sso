dcc0ae87a301d51d993f9e5623140c3a4dceaafe
wuhaitao <wuhaitao@bdfint.com>
2017-12-28 14:09:31 +0800

    添加热更新插件 , 关闭pscache

--- src/main/java/com/dzlb/sso/controller/UserController.java
+++ src/main/java/com/dzlb/sso/controller/UserController.java
@@ -1,9 +1,12 @@
 package com.dzlb.sso.controller;
 
+import com.dzlb.sso.entity.User;
 import com.dzlb.sso.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 /**
  * Created by wuhai on 2017/12/20.
@@ -14,6 +17,12 @@ public class UserController {
 
     @Autowired
     private UserService userService;
+
+
+    @RequestMapping("/selectAll")
+    public List<User> selectAll(){
+        return userService.selectAll();
+    }
 
 
 
